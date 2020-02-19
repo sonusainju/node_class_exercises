1cfbce142ced42b4490b3a10b51638306f851704
sonusainju <sainjusonu@gmail.com>
2020-02-18 19:29:24 -0600

    JS exercises varialbles

---
+++ JS/index4.js
@@ -0,0 +1,5 @@
+var firstName = "Sonu";
+var lastName = "Sainju";
+var age = 10;
+var date ="01/01/1990";
+var address = "Regina"
---
+++ JS/index5.js
@@ -0,0 +1,14 @@
+var firstName = "Sonu";
+var lastName = "Sainju";
+var age = 10;
+var date ="01/01/1990";
+var address = "Regina"
+
+console.log("firstName ", firstName);
+console.log("lastName ", lastName);
+
+console.log("age ", age);
+console.log("date " + date);
+console.log("address " + address);
+
+
