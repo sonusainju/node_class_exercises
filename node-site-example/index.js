1ad3c59c099b28ec7a2e2d2d649bc15b39c5d26e
sonusainju <sainjusonu@gmail.com>
2020-03-27 16:02:31 -0600

    More to the node site

--- node-site-example/index.js
+++ node-site-example/index.js
@@ -1,28 +1,63 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+//const superheroes = require('./superheroes.js');
+
+
+app.use(express.static('public'));
+app.set('view engine', 'pug');
+
+const superheroes = [
+  { id: 1, name: "SPIDER-MAN", image: "spiderman.jpg" },
+  { id: 2, name: "CAPTAIN MARVEL", image: "captainmarvel.jpg" },
+  { id: 3, name: "HULK", image: "hulk.jpeg" },
+  { id: 4, name: "THOR", image: "thor.jpg" },
+  { id: 5, name: "IRON MAN", image: "ironman.jpg" },
+  { id: 6, name: "DAREDEVIL", image: "daredevil.jpg" },
+  { id: 7, name: "BLACK WIDOW", image: "blackwidow.jpg" },
+  { id: 8, name: "CAPTAIN AMERICA", image: "captanamerica.jpeg" },
+  { id: 9, name: "WOLVERINE", image: "wolverine.jpg" }
+];
+
+app.get("/", (req, res) => {
+  
+  res.render("index", { superheroes: superheroes });
+});
+
+app.get("/superheroes/:id", (req, res) => {
+  const selectedId = req.params.id;
+
+  let selectedSuperhero = superheroes.filter(superhero => {
+    return superhero.id === +selectedId;
+  });
+
+  selectedSuperhero = selectedSuperhero[0];
+
+  res.render("superhero", { superhero: selectedSuperhero });
+});
+
+app.post("/superheroes/", urlencodedParser, (req, res) => {
+  const newId = superheroes[superheroes.length - 1].id + 1;
+  const newSuperHero = {
+    id: newId,
+    name: req.body.superhero.toUpperCase(),
+    image: "lukecage.jpg"
+  };
+  superheroes.push(newSuperHero);
+
+  res.redirect("/");
+});
+
+app.use(function(req, res, next) {
+  res.status(404).render("404", {url: req.url});
+});
 
 app.listen(3000, function() {
-    console.log("Example app listening on port 3000!");
-  });
 
-  app.set('view engine', 'pug');
+  console.log("Example app listening on port 3000!");
+});
 
-  app.get("/", (req, res) => {
-    const superheroes = [
-      { name: "SPIDER-MAN", image: "spiderman.jpg" },
-      { name: "CAPTAIN MARVEL", image: "captainmarvel.jpg" },
-      { name: "HULK", image: "hulk.jpeg" },
-      { name: "THOR", image: "thor.jpg" },
-      { name: "IRON MAN", image: "ironman.jpg" },
-      { name: "DAREDEVIL", image: "daredevil.jpg" },
-      { name: "BLACK WIDOW", image: "blackwidow.jpg" },
-      { name: "CAPTAIN AMERICA", image: "captanamerica.jpeg" },
-      { name: "WOLVERINE", image: "wolverine.jpg" }
-    ];
-  
-    res.render("index", { superheroes: superheroes });
-  });
 
-  app.use(express.static('public'));
 
---
+++ node-site-example/superheroes.js
@@ -0,0 +1,11 @@
+const superheroes = [
+    { id: 1, name: "SPIDER-MAN", image: "spiderman.jpg" },
+    { id: 2, name: "CAPTAIN MARVEL", image: "captainmarvel.jpg" },
+    { id: 3, name: "HULK", image: "hulk.jpeg" },
+    { id: 4, name: "THOR", image: "thor.jpg" },
+    { id: 5, name: "IRON MAN", image: "ironman.jpg" },
+    { id: 6, name: "DAREDEVIL", image: "daredevil.jpg" },
+    { id: 7, name: "BLACK WIDOW", image: "blackwidow.jpg" },
+    { id: 8, name: "CAPTAIN AMERICA", image: "captanamerica.jpeg" },
+    { id: 9, name: "WOLVERINE", image: "wolverine.jpg" }
+  ];
