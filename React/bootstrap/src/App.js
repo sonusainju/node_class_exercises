0f79c3f89338e7fe18a91646d4dcad05bc9f909f
sonusainju <sainjusonu@gmail.com>
2020-04-09 20:51:49 -0600

    React apps

---
+++ React/bootstrap/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css//bootstrap.min.css';
+import {Alert} from 'react-bootstrap'
+
+
+function App() {
+  return (
+    <div>
+      <Alert variant="primary">
+        My first Bootstrap component
+      </Alert>
+
+
+    </div>
+  );
+}
+
+export default App;
